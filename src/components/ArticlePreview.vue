9dd900713a61a1a0a5c22c84d34fbab49f62f444
6lszxz <ediamonds@126.com>
2023-04-03 22:18:10 +0800

    (11)-2

--- src/components/ArticlePreview.vue
+++ src/components/ArticlePreview.vue
@@ -1,19 +1,34 @@
 <script setup>
+import axios from 'axios';
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
-defineProps(['mainText']);
+const article = ref({
+    title: '默认文章',
+    id: '0000000000000000',
+    author: '0',
+    createTime: 0,
+    content: '文章内容',
+    likes: 0,
+});
+const router = useRouter()
 
-let like = ref(0);
-function likeUp(){
-    like.value++
+axios.get('/server/getRandomArticle')
+.then((response)=>{
+    article.value = response.data;
+})
+
+function linkToArticle(){
+    router.push(`/article/${article.value.id}`)
 }
+
 </script>
 
 <template>
-    <div>
-        <slot/>
-        {{ mainText }}
-        <button @click="likeUp">点赞数：{{ like }}</button>
+    <div @click="linkToArticle">
+        {{ article.title }}
+        {{ article.content }}
+        
     </div>
 </template>
 
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -3,20 +3,12 @@
 import { ref } from 'vue';
 import ArticlePreview from '../components/ArticlePreview.vue';
 
-let mainText= ref(0);
-setTimeout(()=>{
-    mainText.value = 1;
-},1000)
 
-let blogTitles = [1,2,3,4,5,6,7,8,9]
 </script>
 
 <template>
     <header>
-        <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125" />
-        <div v-for="blogTitle in blogTitles">
-            <ArticlePreview>{{ blogTitle }}</ArticlePreview>
-        </div>
+        <ArticlePreview></ArticlePreview>
         <div class="wrapper">
         </div>
     </header>
