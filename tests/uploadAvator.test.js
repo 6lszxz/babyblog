48fcc5ed0bde21b6f478d2852255622aea2da873
6lszxz <ediamonds@126.com>
2023-04-13 22:20:19 +0800

    (16)-2

---
+++ tests/uploadAvator.test.js
@@ -0,0 +1,17 @@
+import { describe, expect, it } from "vitest";
+import { mount } from "@vue/test-utils";
+
+import uploadAvatar from '../src/components/uploadAvatar.vue'
+
+// 描述一组测试，包括名称和包括的测试项目
+describe('test uploadAvatar',()=>{
+    // 一个测试
+    it('should render',()=>{
+        // 需要将组件实例化之后才可以进行测试
+        const wrapper = mount(uploadAvatar,{
+            
+        })
+        // 看看组件里面包不包括'头像'v这个字符串
+        expect(wrapper.text()).contain('头像')
+    })
+})
